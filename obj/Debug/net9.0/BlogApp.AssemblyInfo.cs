c56a01d21a2a2f26a921a566730d2b7023431249
chamodi <chamodipiyadasa@gmail.com>
2025-07-31 20:47:10 +0530

    modify

--- obj/Debug/net9.0/BlogApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/BlogApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b4581735e06bc02848c3b74aa4522780cf702fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2404f65fdf4049bf31a239a1743387316518a08c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
